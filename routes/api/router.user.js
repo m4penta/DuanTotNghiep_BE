d26e757c640fd70cd33e91e62448fa77d20e5f08
m4penta <121990777+bikujijio@users.noreply.github.com>
2023-07-23 12:16:21 +0700

    update: userupdate-userCanUpdate

--- routes/api/router.user.js
+++ routes/api/router.user.js
@@ -5,7 +5,8 @@ const userController = require('../../app/controllers/controller.user');
 Router.post('/login', authController.login);
 Router.post('/register', authController.register);
 //update user FE(
-Router.get('/user/getAllUsers', authController.getAllUsers); //get all user detail
+//Router.get('/user/getAllUsers', authController.getAllUsers); //get all user detail
 Router.get('/user/:id/getuser', userController.userGetByid); // get user by id
 Router.put('/user/:id/update', userController.updateUserById); // update user by id
+Router.post('/user/edit', authController.apiUserPostEdit);// user can update detail
 module.exports = Router;
