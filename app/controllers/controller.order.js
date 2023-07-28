f7c03e21e71524eee4d138acf4abbd9a701b67f9
m4penta <121990777+bikujijio@users.noreply.github.com>
2023-07-28 13:42:15 +0700

    delete store

--- app/controllers/controller.order.js
+++ app/controllers/controller.order.js
@@ -99,13 +99,40 @@ class OrderController {
     }
   };
   //DELETE  api/store/delete/:id
-  deleteOrderById = async (req, res, next) => {
-    const { id } = req.params;
-    const query = {
-      _id: id,
-    };
-    await orderModel.findByIdAndRemove(query).exec();
-    return res.json({ message: 'delete succesfully' });
+  // deleteOrderById = async (req, res, next) => {
+  //   const { id } = req.params;
+  //   const query = {
+  //     _id: id,
+  //   };
+  //   await orderModel.findByIdAndRemove(query).exec();
+  //   return res.json({ message: 'delete succesfully' });
+  // };
+  deleteOrderById  = async (req, res, next) => {
+    try {
+      console.log(req.headers);
+      if (req.headers['authorization']) {
+        const userToken = req.headers['authorization'].split(' ')[1];
+        const { _id: userId } = jwt.decode(userToken);
+        const orderId = req.params.orderId; // Lấy orderId từ tham số URL
+  
+        // Kiểm tra orderId nếu cần thiết (vd: validate orderId có hợp lệ)
+  
+        // Xóa đơn hàng dựa trên orderId và userId
+        const deletedOrder = await orderModel.findOneAndDelete({
+          _id: orderId,
+          userId: userId,
+        });
+  
+        if (!deletedOrder) {
+          return res.status(404).json({ message: 'Order not found' });
+        }
+  
+        res.status(200).json({ message: 'Order deleted successfully' });
+      }
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
   };
   // get
   findOrder = async (req, res, next) => {
